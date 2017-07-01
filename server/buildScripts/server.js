23c419caaa08ed9ab9d596cba065a0a1ed1d2508
Nick Lee <nicklee100@gmail.com>
2017-07-01 12:54:58 -0400

    development express server running with babel, rendering react

---
+++ server/buildScripts/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import webpack from 'webpack';
+import config from '../../webpack.config.dev';
+
+const compiler = webpack(config);
+
+const port = 3000;
+const app = express()
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+app.listen(port, function (error) {
+  if(error) {
+    console.log(error)
+  } else {
+     console.log('app running on port:', port)
+  }
+})
---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+import webpack from 'webpack'
+import path from 'path'
+
+export default {
+  devtool: 'inline-source-map',
+
+  entry: [
+    path.resolve(__dirname, 'client/index.js')
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] }
+    ]
+  }
+}
