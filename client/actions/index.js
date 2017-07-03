a4d8f1f0dd06bb50f7c9f15c780e72a572d2845a
Nick Lee <nicklee100@gmail.com>
2017-07-02 22:52:51 -0400

    added fetch forcast action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
 export const FETCH_LOCATION_SUCCESS = 'FETCH_LOCATION_SUCCESS'
+export const FETCH_INITIAL_FORCAST = 'FETCH_INITIAL_FORCAST'
 
 export function fetchLocationSuccess(location) {
   return {
     type: FETCH_LOCATION_SUCCESS,
     location
+  }
+}
+
+export function fetchForcast(forcast) {
+  return {
+    type: FETCH_INITIAL_FORCAST,
+    forcast
   }
 }
